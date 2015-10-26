dd085b4be0938b04b7eaf455991b62e29b738620
Jerry Kuru <jerrykuru@gmail.com>
2015-10-26 00:15:36 -0400

    Removed the API key

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Popular-Movies-App
 Popular Movies App 
+In the MovieConstants file please add your API key 
+
--- app/src/main/java/com/popularmovie/android/appprotfolio/popularmovie/MovieConstants.java
+++ app/src/main/java/com/popularmovie/android/appprotfolio/popularmovie/MovieConstants.java
@@ -3,5 +3,5 @@ package com.popularmovie.android.appprotfolio.popularmovie;
 
 public interface MovieConstants {
        String CUSTOM_LISTING = "movieObj";
-       String ApiKey = "7c5abefc53acec71067d4859c75dbd0f";
+       String ApiKey = "Please add your key here";
 }
